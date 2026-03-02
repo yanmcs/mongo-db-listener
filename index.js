7697be440af413900e7ca7c36f6f1f09171d7ca7
Yan Menezes Carneiro da Silva <yamcs@me.com>
2026-03-02 17:24:25 -0300

    better

--- index.js
+++ index.js
@@ -648,7 +648,7 @@ async function start() {
             const updatedFields = Object.keys(change.updateDescription.updatedFields || {});
             const removedFields = change.updateDescription.removedFields || [];
             
-            const ignoredPrefixes = ['updatedAt', 'talkTime', 'control'];
+            const ignoredPrefixes = ['updatedAt', 'talkTime', 'control', 'lastActive'];
             const hasMeaningfulUpdates = updatedFields.some(field => 
               !ignoredPrefixes.some(prefix => field === prefix || field.startsWith(`${prefix}.`))
             ) || removedFields.length > 0;
